c1536a8f7270b274bdddb811560eeaa0e9ee8b9a
Gabriel Capetini Sant'Anna <87335853+GabrielCapetini123@users.noreply.github.com>
2022-01-06 13:05:09 -0300

    Add files via upload

---
+++ Reservation.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp20.Entities
+{
+    class Reservation
+    {
+        public int roomNumbers { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+
+        public Reservation()
+        {
+        }
+
+        public Reservation(int roomNumbers, DateTime checkIn, DateTime checkOut)
+        {
+            this.roomNumbers = roomNumbers;
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+
+        public int Duration()
+        {
+            TimeSpan duration = CheckOut.Subtract(CheckIn);
+            return (int)duration.TotalDays;
+        }
+
+        public void UpdateDates(DateTime checkIn, DateTime checkOut)
+        {
+            checkIn = checkIn;
+        }
+        public override string ToString()
+        {
+            return "Room "
+                + roomNumbers
+                + ", "
+                + "Checkin : "
+                + CheckIn.ToString("dd/MM/yyyy")
+                + (", check-out : ")
+                + CheckOut.ToString("dd/MM/yyyy")
+                + ", "
+                + Duration()
+                + " nights";
+
+        }
+    }
+}
